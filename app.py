132dfe1cf8bf493378a88c63fbef8c7e35b36c54
Francisco Flores <is728238@iteso.mx>
2024-05-27 14:02:54 -0600

    Heroku

--- app.py
+++ app.py
@@ -32,5 +32,13 @@ def contact():
 
     return render_template('contact.html')
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
